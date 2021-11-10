f387b803ed2bde5bde798e608b4423e9c91cb6a1
Eduardo Reyna <49138106+EduVR19@users.noreply.github.com>
2021-11-10 09:32:41 -0600

    Se quitó flag, se corrigio 0 a M, comentarios

--- 1868879_Heuristico.py
+++ 1868879_Heuristico.py
@@ -123,7 +123,7 @@ arreglo_array = dataframe3.to_numpy()
 
 lista_final = []
 lista_temp = [0]
-indice_cliente = 0
+producto = 0
 tiempo_periodo_temp = 0
 productos = []
 # Listas necesarias para contener datos finales y de control
@@ -131,38 +131,38 @@ productos = []
 
 print()
 print("Datos")
-n_nodos = (values_array[0][1] + 1)
-print( "No. de productos: ", (values_array[0][1]) )
+n_productos = (values_array[0][1] + 1)
+print( "No. de productos:", (values_array[0][1]) )
 # Muestra la cantidad de nodos
 
 M = (values_array[1][1])
-print( "Tiempo de Mantenimiento M: ", values_array[1][1])
+print( "Tiempo de Mantenimiento M:", values_array[1][1])
 # Muestra el valor de M
 
 T = (values_array[2][1])
-print("Tiempo de Periodo T: ", values_array[2][1])
+print("Tiempo de Periodo T:", values_array[2][1])
 # Muestra el valor de T
 
 C = T - M
-print("Capacidad maxima C: ", C)
+print("Capacidad maxima C:", C)
 # Capacidad de cada ciclo
 
 # imprimir pruductos
 print()
 for x in range(len(productos_array)):
     productos.append(productos_array[x][1])
-print("Tiempo por producto")
+print("Tiempo de Preparación por Producto")
 for i in range( len( productos ) ):
     print(f"{i + 1} {productos[i]}")
 productos.insert(0, M)
 # Agrega a la lista de productos el arreglo respectivo
 # e inserta al principio el valor de M
 
-matrizTiempos = arreglo_array
+matrizTiempoPreparacion = arreglo_array
 print()
-print("Matriz de Tiempos")
+print("Matriz de Tiempos de Preparación")
 for i in range( len(productos ) ):
-    print(f"{i} {matrizTiempos[i]}")
+    print(f"{i} {matrizTiempoPreparacion[i]}")
 # Simplemente muestra la matriz de tiempos
 ################## Fin - Lectura de Datos ################## 
 
@@ -170,9 +170,9 @@ for i in range( len(productos ) ):
 
 
 ################## Inicio - Generar Secuenciación de productos ################## 
-lista_final = []
-lista_temp = [0]
-indice_cliente = 0
+lista_final         = []
+lista_temp          = [0]
+producto            = 0
 tiempo_periodo_temp = 0
 
 # Función para verificar si ya se agregaron todos los productos
@@ -181,48 +181,53 @@ def verifyProducts():
     for i in range( len(lista_temp) ):
         if ( i in lista_temp):
             count = count + 1
-        
-    if(count == n_nodos):
-            return True
+    if(count == n_productos):
+            return False
     else:
-        return False    
+        return True    
 
-flag = False
-
-while( not flag ): # El ciclo dura n_nodos - 1 para así reservar el último puesto para el 
-    # cliente inicial
-    costo_min = max( matrizTiempos[indice_cliente] ) # Se encuentra el valor máximo de la fila
-    for i in range(n_nodos): # Aquí se recorren todos los clientes de la fila actual
-        # Se compara el costo de cada cliente para encontrar el valor mínimo y se valida también que 
-        # el costo no sea 0
-        if ( matrizTiempos[indice_cliente][i] < costo_min):
-            # Se verifica que el cliente no se encuentre ya en la lista del tour
-            # Si se encuentra se continua fuera del if-else o si i = j (diagonal de 0s) se excluye
-            if ( ( i in lista_temp ) or ( i == indice_cliente) ):
+while( verifyProducts() ): # El ciclo dura hasta que todos los productos se haya agregado a la secuencia
+    tiempo_min = max( matrizTiempoPreparacion[producto] ) # Se encuentra el valor máximo de la fila
+    for i in range(n_productos): # Aquí se recorren todos los productos de la fila actual
+        # Se compara el tiempo de cada producto para encontrar el valor mínimo
+        if ( matrizTiempoPreparacion[producto][i] < tiempo_min):
+            # Se verifica que el producto no se encuentre ya en la lista del tour
+            # Se verifica el caso cuando i = j (diagonal de 0s) 
+            # Si se encuentra se continua fuera del if-else, excluyéndolo
+            if ( ( i in lista_temp ) or ( i == producto) ):
                 continue
             # Si no se encuentra se considera para poder encontrar el valor mínimo
             else:
-                costo_min = matrizTiempos[indice_cliente][i] # Se guarda el valor del costo mínimo en la variable
-                i_costo_minimo = i 
+                tiempo_min = matrizTiempoPreparacion[producto][i] # Se guarda el valor del tiempo mínimo en la variable
+                i_tiempo_minimo = i 
 
+    # Se hace una suma de tiempo para verificar que no se pase del tiempo límite
+                # Tiempo de preparación del producto actual al producto a agregar 
+    sumaTiempos = matrizTiempoPreparacion[producto][i_tiempo_minimo] 
+                # Tiempo de preparación del producto a agregar 
+    sumaTiempos += productos[i_tiempo_minimo] 
+                # Tiempo de preparación del producto a agregar al Mantenimiento (0)
+    sumaTiempos += matrizTiempoPreparacion[i_tiempo_minimo][0]
 
-    sumaTiempos = matrizTiempos[indice_cliente][i_costo_minimo] + productos[i_costo_minimo] + matrizTiempos[0][i_costo_minimo]
-
+    # Verificar que no pase del tiempo límite, sumando el tiempo del periodo actual
+    # + el tiempo generado si se agrega el nuevo producto
     if( ( tiempo_periodo_temp + sumaTiempos ) < C):
-        # El número del cliente sería igual a su índice (matriz)
-        lista_temp.append(i_costo_minimo) # Y ya se agrega el cliente con el menor costo a la lista del 
-        # tour después de las validaciones
-        tiempo_periodo_temp = tiempo_periodo_temp + ( matrizTiempos[indice_cliente][i_costo_minimo] + productos[i_costo_minimo] )
-        indice_cliente = i_costo_minimo # Se guarda el índice del último cliente que se ha añadido para 
-        # encontrar el nuevo cliente
+        lista_temp.append(i_tiempo_minimo) # Y ya se agrega el producto con el menor tiempo a la secuencia
+                            # Tiempo del periodo actual
+        tiempo_periodo_temp = tiempo_periodo_temp
+                            # Tiempo de preparación del producto actual al producto a agregar  
+        tiempo_periodo_temp += matrizTiempoPreparacion[producto][i_tiempo_minimo] 
+        tiempo_periodo_temp += productos[i_tiempo_minimo]
+        producto = i_tiempo_minimo # Se guarda el índice del último producto que se ha añadido para 
+        # encontrar el nuevo producto
         
     else:
-        lista_temp.append(0) # Se agrega el nodo 0 (Mantenimiento) debido a que ya no caben más productos
-        indice_cliente = 0 # Se guarda el índice con 0 (Mantenimiento)
+        lista_temp.append(0) # Se agrega el producto 0 (Mantenimiento) debido a que ya no caben más productos
+        producto = 0 # Se guarda el índice con 0 (Mantenimiento)
         tiempo_periodo_temp = 0 # Se reinicia el tiempo del periodo
+# Fin del for para todos los productos
 
-    flag = verifyProducts()
-# Fin del for para todos los nodos
+# Se quita el mantenimiento (0) al inicio de la lista
 lista_temp.pop(0)
 lista_temp.append(0)
 ################## Fin - Generar Secuenciación de productos ################## 
@@ -231,38 +236,38 @@ lista_temp.append(0)
 
 
 
-################## Inicio - Calcular Costo Total ################## 
-# Inicializar variable para el costo total
-costo_total = 0
-costo_total = costo_total + matrizTiempos[ 0 ][ lista_temp[0] ]
-# Para cada nodo se encuentra su costo con el nodo siguiente
-for nodo in range( len(lista_temp) ):
-    if (nodo == len(lista_temp) - 1): #Si es igual al último índice se sale del loop
+################## Inicio - Calcular Tiempo Total ################## 
+# Inicializar variable para el tiempo total
+tiempo_total = 0
+tiempo_total = tiempo_total + matrizTiempoPreparacion[ 0 ][ lista_temp[0] ]
+# Para cada producto se encuentra su tiempo con el producto siguiente
+for producto in range( len(lista_temp) ):
+    if (producto == len(lista_temp) - 1): #Si es igual al último índice se sale del loop
         break
-    # Se consulta la matriz de tiempos para obtener el costo total del 
+    # Se consulta la matriz de tiempos para obtener el tiempo total del 
     # Tour TSP Final
-    costo_total = costo_total + matrizTiempos[ lista_temp[nodo] ][ lista_temp[nodo + 1] ]
+    tiempo_total = tiempo_total + matrizTiempoPreparacion[ lista_temp[producto] ][ lista_temp[producto + 1] ]
 
 
 # Calcular cuántos mantenimientos hay en la lista
 countM = 0
-for nodo in range( len(lista_temp) ):
-    if( lista_temp[nodo] == 0 ):
+for producto in range( len(lista_temp) ):
+    if( lista_temp[producto] == 0 ):
         countM = countM + 1
+tiempo_total = tiempo_total + (M * countM)
 
-costo_total = costo_total + (M * countM)
-
-costo_total = costo_total + sum(productos) - M
-################## Fin - Calcular Costo Total ################## 
+# Agregar el tiempo de los productos
+tiempo_total = tiempo_total + sum(productos) - M
+################## Fin - Calcular Tiempo Total ################## 
 
 
 
 ################## Inicio - Impresion de resultados ################## 
-# imprimir matriz
 print()
 print("Resultados")
 print("Secuencia de productos")
 print(lista_temp)
-print("Costo total")
-print(costo_total)
+print("Tiempo total")
+print(tiempo_total)
+print()
 ################## Fin - Impresion de resultados ################## 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SchedulingProblemHeuristic
+# Scheduling Problem Heuristic
+## Problema de secuenciación de tareas para una máquina con un tiempo de trabajo y un tiempo de mantenimiento que procesa con un determinado número de productos con su tiempo de preparación
